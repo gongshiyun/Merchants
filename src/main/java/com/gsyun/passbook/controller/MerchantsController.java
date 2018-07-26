67227e657b67160d7feced66005cecaaec8aebec
gongshiyun <437010518@qq.com>
2018-07-26 18:25:29 +0800

    商户投放子系统 - 商户服务controller

---
+++ src/main/java/com/gsyun/passbook/controller/MerchantsController.java
@@ -0,0 +1,44 @@
+package com.gsyun.passbook.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.gsyun.passbook.service.IMerchantsService;
+import com.gsyun.passbook.vo.CreateMerchantsRequest;
+import com.gsyun.passbook.vo.Response;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author gongshiyun
+ * @Description 商户服务Controller
+ * @date 2018/7/26
+ */
+@Slf4j
+@RestController
+@RequestMapping("/merchants")
+public class MerchantsController {
+
+    /**
+     * 商户服务接口
+     */
+    private final IMerchantsService merchantsService;
+
+    @Autowired
+    public MerchantsController(IMerchantsService merchantsService) {
+        this.merchantsService = merchantsService;
+    }
+
+    @ResponseBody
+    @PostMapping("/create")
+    public Response createMerchants(@RequestBody CreateMerchantsRequest request) {
+        log.info("CreateMerchants: " + JSON.toJSONString(request));
+        return merchantsService.createMerchants(request);
+    }
+
+    @ResponseBody
+    @GetMapping("/{id}")
+    public Response buildMerchantsInfoById(@PathVariable Integer id) {
+        log.info("buildMerchantsInfoById:" + id);
+        return merchantsService.buildMerchantsInfoById(id);
+    }
+}
